3f093dd6a1df3c9bec93bf759fd701391cc10137
noey <130928282+gobok0305@users.noreply.github.com>
2025-07-23 13:40:52 +0900

    AStar

--- Assets/3. Unity Book/02. Scripts/Path Finding/AStar.cs
+++ Assets/3. Unity Book/02. Scripts/Path Finding/AStar.cs
@@ -1,16 +1,89 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class AStar : MonoBehaviour
+public class AStar
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static PriorityQueue openList; // 방문할수 있는 후보
+    public static PriorityQueue closedList; // 이미 방문한 노드
+
+    //휴리스틱(최단거리를 추측) 기반 최단거리 뽑아내기
+    private static float HeuristicEstimateCost(Node curNode, Node endNode)
     {
-        
+        Vector3 cost = curNode.pos - endNode.pos;
+        return cost.magnitude;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static List<Node> FindPath(Node startNode, Node endNode)
     {
-        
+        openList = new PriorityQueue();
+        openList.Push(startNode);
+        startNode.nodeTotalCost = 0f;
+        startNode.estimateCost = HeuristicEstimateCost(startNode,endNode);
+        closedList = new PriorityQueue();
+        Node node = null;
+
+        while (openList.Length != 0)
+        {
+            node = openList.First();
+
+            if (node.pos == endNode.pos) // 목적지에 도착
+            {
+                return CalculatePath(node);
+            }
+
+            List<Node> neighbors = new List<Node>();
+            GridManager.Instance.GetNeighbors(node, neighbors);
+
+            for (int i = 0; i < neighbors.Count; i++)
+            {
+                Node neighborNode = neighbors[i];
+
+                if (!closedList.Contains(neighborNode))
+                {
+                    float cost = HeuristicEstimateCost(node, neighborNode);
+                    float totalCost = node.nodeTotalCost + cost;
+
+                    float neighborNodeEstCost = HeuristicEstimateCost(neighborNode, endNode);
+
+                    neighborNode.nodeTotalCost = totalCost;
+                    neighborNode.parent = node;
+                    neighborNode.estimateCost = totalCost + neighborNodeEstCost;
+
+                    if (!openList.Contains(neighborNode))
+                    {
+                        openList.Push(neighborNode);
+                    }
+                }
+            }
+            closedList.Push(node);
+            openList.Remove(node);
+
+        }
+
+        if (node.pos != endNode.pos)
+        {
+            Debug.LogError("Destination Path Not Found");
+
+            return null;
+        }
+
+        return CalculatePath(node);
+
+    }
+
+    private static List<Node> CalculatePath(Node node)
+    {
+        List<Node> list = new List<Node>();
+
+        while (node != null)
+        {
+            list.Add(node);
+            node = node.parent;
+        }
+
+        list.Reverse();
+
+        return list;
     }
 }
--- Assets/3. Unity Book/02. Scripts/Path Finding/AStarMover.cs
+++ Assets/3. Unity Book/02. Scripts/Path Finding/AStarMover.cs
@@ -1,16 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AStarMover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Transform startPos, endPos;
+    public Node startNode, endNode;
+
+    public List<Node> pathList = new List<Node>();
+
+    public GameObject startCube, endCube;
+
+    private void Start()
     {
-        
+        GetPath();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GetPath()
     {
-        
+        startPos = startCube.transform;
+        endPos = endCube.transform;
+
+        // astar에게  start와 end지점을  알려주고 길을 알려달라고 설정
+        int startIndex = GridManager.Instance.GetGridIndex(startPos.position);
+        int startRow = GridManager.Instance.GetRow(startIndex);
+        int startCol = GridManager.Instance.GetColumn(startIndex);
+        startNode = GridManager.Instance.nodes[startRow, startCol];
+
+        int endIndex = GridManager.Instance.GetGridIndex(endPos.position);
+        int endRow = GridManager.Instance.GetRow(endIndex);
+        int endCol = GridManager.Instance.GetColumn(endIndex);
+        endNode = GridManager.Instance.nodes[endRow, endCol];
+
+        // start에서 end까지의 방문한 node list
+        pathList = AStar.FindPath(startNode, endNode);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (pathList == null)
+            return;
+
+        if (pathList.Count > 0)
+        {
+            int index = 1;
+            foreach (Node node in pathList)
+            {
+                Node nextNode = pathList[index];
+                Debug.DrawLine(node.pos, nextNode.pos, Color.green);
+                index++;
+            }
+
+        }
     }
 }
