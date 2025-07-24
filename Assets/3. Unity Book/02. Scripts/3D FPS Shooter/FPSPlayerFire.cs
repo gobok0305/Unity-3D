8a379d46dc6c195198ff23fa9ab10f1968690065
noey <130928282+gobok0305@users.noreply.github.com>
2025-07-24 15:15:50 +0900

    3D FPS Shooter (532~)

--- Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerFire.cs
+++ Assets/3. Unity Book/02. Scripts/3D FPS Shooter/FPSPlayerFire.cs
@@ -1,66 +1,104 @@
+using TMPro;
 using UnityEngine;
 
 public class FPSPlayerFire : MonoBehaviour
 {
+    private enum WeaponMode { Normal, Sniper }
+    private WeaponMode wMode;
+
     public GameObject firePosition;
     public GameObject bombFactory;
     public GameObject bulletEffect;
     private Animator anim;
     private ParticleSystem ps;
 
-    public float throwPower = 15;
+    public TextMeshProUGUI wModeText;
+
+    public float throwPower = 15f;
     public int weaponPower = 5;
 
+    private bool ZoomMode = false;
 
-    private void Start()
+    void Start()
     {
         anim = GetComponentInChildren<Animator>();
         ps = bulletEffect.GetComponent<ParticleSystem>();
+
+        wMode = WeaponMode.Normal;
     }
 
-
-    private void Update()
+    void Update()
     {
         if (FPSGameManager.Instance.gState != FPSGameManager.GameState.Run)
             return;
 
-        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 클릭
+        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭
         {
             if (anim.GetFloat("MoveMotion") == 0)
-            {
                 anim.SetTrigger("Attack");
-            }
-            
+
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-            //출발점                             //방향
             RaycastHit hitInfo = new RaycastHit();
 
             if (Physics.Raycast(ray, out hitInfo))
             {
-                // raycast를 맞은 대상이 enemy가 맞은 경우
-                if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                if (hitInfo.transform.gameObject.layer == LayerMask.NameToLayer("Enemy")) // Raycast를 Enemy가 맞은 경우
                 {
                     EnemyFSM eFSM = hitInfo.transform.GetComponent<EnemyFSM>();
                     eFSM.HitEnemy(weaponPower);
                 }
-                else // raycast를 맞은 대상이 enemy가 아닌 경우
+                else // Raycast를 맞은 대상이 Enemy가 아닌 경우
                 {
                     bulletEffect.transform.position = hitInfo.point;
-                    bulletEffect.transform.forward = hitInfo.normal; //파티클이 tranform 앞으로 나오도록!
+                    bulletEffect.transform.forward = hitInfo.normal;
 
                     ps.Play();
                 }
-        
             }
         }
 
-        if (Input.GetMouseButtonDown(1)) //마우스 오른쪽 버튼 클릭
+        if (Input.GetMouseButtonDown(1)) // 마우스 오른쪽 버튼 클릭
         {
-            GameObject bomb = Instantiate(bombFactory);
-            bomb.transform.position = firePosition.transform.position;
+            switch (wMode)
+            {
+                case WeaponMode.Normal: // 일반 모드일 때 마우스 오른쪽 -> 수류탄 투척
+                    GameObject bomb = Instantiate(bombFactory);
+                    bomb.transform.position = firePosition.transform.position;
 
-            Rigidbody rb = bomb.GetComponent<Rigidbody>();
-            rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+                    Rigidbody rb = bomb.GetComponent<Rigidbody>();
+                    rb.AddForce(Camera.main.transform.forward * throwPower, ForceMode.Impulse);
+                    break;
+                case WeaponMode.Sniper: // 저격 모드일 때 마우스 오른쪽 -> 확대/축소 조준경
+                    // if (!ZoomMode)
+                    // {
+                    //     Camera.main.fieldOfView = 15f;
+                    //     ZoomMode = true;
+                    // }
+                    // else
+                    // {
+                    //     Camera.main.fieldOfView = 60f;
+                    //     ZoomMode = false;
+                    // }
+
+                    float fov = ZoomMode ? 60f : 15f;
+                    Camera.main.fieldOfView = fov;
+                    ZoomMode = !ZoomMode;
+                    break;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            wMode = WeaponMode.Normal;
+            Camera.main.fieldOfView = 60f;
+
+            wModeText.text = "Normal Mode";
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            wMode = WeaponMode.Sniper;
+
+            wModeText.text = "Sniper Mode";
         }
     }
 }
